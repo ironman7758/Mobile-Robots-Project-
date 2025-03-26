c19884889936ae9b9c6e12bdd526a4222b107c98
duckTapw <lachlan.lego7@gmail.com>
2025-03-26 11:52:47 +0800

    first commit

---
+++ docker/using.md
@@ -0,0 +1,8 @@
+-- build container
+docker build -t <name> .
+
+--run and attach on terminal
+docker run -it <name> bash
+
+-- source ros if lost
+source /opt/ros/jazzy/setup.bash
---
+++ src/my_package2/src/my_node2.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  rclcpp::init(argc, argv);
+  auto new_node = std::make_shared<rclcpp::Node>("new_node");
+  rclcpp::spin(new_node);
+  
+  printf("hello world my_package2 package\n");
+  return 0;
+}
