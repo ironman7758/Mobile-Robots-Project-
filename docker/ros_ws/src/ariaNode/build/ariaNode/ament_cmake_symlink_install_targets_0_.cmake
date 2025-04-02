50c35611f4b76345f0dce12203995c83466999bf
duckTapw <lachlan.lego7@gmail.com>
2025-04-02 10:58:57 +0800

    Added colcon build to docker file

--- docker/ros_ws/src/ariaNode/build/ariaNode/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/workspace/ros_ws/src/ariaNode/build/ariaNode/ariaNode" "TARGETS" "ariaNode" "DESTINATION" "lib/ariaNode")
--- docker/ros_ws/src/ariaNode/build/ariaNode/colcon_command_prefix_test.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/workspace/ros_ws/src/ariaNode/install/ariaNode/share/ariaNode/package.sh"
